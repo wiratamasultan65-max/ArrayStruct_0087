2bbf7697d1a800947e0521adf8182f06ed8bf4e9
wiratamasultan65-max <wiratamasultan65@gmail.com>
2026-04-07 14:56:11 +0700

    Deklarasi Array dengan inisialisasi

--- Array.cpp
+++ Array.cpp
@@ -1,3 +1,8 @@
 #include <iostream>
 using namespace std;
- 
+
+int main() {
+    // --- BAGIAN DARI GAMBAR 1 ---
+    
+    // Deklarasi Array dengan inisialisasi
+    int dataku[5] = {10, 20, 30, 40, 50};
