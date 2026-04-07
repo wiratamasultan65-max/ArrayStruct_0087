6ed1a2ecb0b8b43ec88ac002c57c27c157343a66
wiratamasultan65-max <wiratamasultan65@gmail.com>
2026-04-07 15:07:16 +0700

    Mengisi dan menampilkan satu persatu

--- Array.cpp
+++ Array.cpp
@@ -26,4 +26,22 @@ int main() {
     cout << "Data Index ke - 2 : "; cin >> datamu[2];
     cout << "Data Index ke - 3 : "; cin >> datamu[3];
     cout << "Data Index ke - 4 : "; cin >> datamu[4];   
-    
+
+     // --- BAGIAN 2: MENGISI & MENAMPILKAN SECARA MANUAL ---
+
+    // Mengisi array datamu satu per satu
+    cout << "Data Index ke - 0 : "; cin >> datamu[0];
+    cout << "Data Index ke - 1 : "; cin >> datamu[1];
+    cout << "Data Index ke - 2 : "; cin >> datamu[2];
+    cout << "Data Index ke - 3 : "; cin >> datamu[3];
+    cout << "Data Index ke - 4 : "; cin >> datamu[4];
+
+    cout << endl;
+
+    // Tampilkan array datamu satu per satu
+    cout << "//Tampilkan array datamu satu per satu" << endl;
+    cout << "Data ke - 1 : " << datamu[0] << endl;
+    cout << "Data ke - 2 : " << datamu[1] << endl;
+    cout << "Data ke - 3 : " << datamu[2] << endl;
+    cout << "Data ke - 4 : " << datamu[3] << endl;
+    cout << "Data ke - 5 : " << datamu[4] << endl;
